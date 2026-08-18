2a18c16eae238585c0f8e598837281152aa358f6
tausif900 <ansaritausif9268@gmail.com>
2026-08-18 19:34:55 +0530

    backend

---
+++ src/test/java/com/pems/backend/PemsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pems.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PemsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
