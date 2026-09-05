bee4f292b9c27f48f922c6bb1b207d927050a257
tausif900 <ansaritausif9268@gmail.com>
2026-09-05 22:04:33 +0530

    PO is in progress

--- src/main/java/com/pems/backend/entity/PurchaseOrder.java
+++ src/main/java/com/pems/backend/entity/PurchaseOrder.java
@@ -1,6 +1,51 @@
 package com.pems.backend.entity;
 
+import java.time.LocalDate;
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class PurchaseOrder {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer poId;
+
+	private String poNumber;
+
+	private LocalDate poDate;
+
+	@OneToMany
+	private List<PurchaseRequest> purchaseRequest;
+
+	private String supplier;
+
+	private String supplierContactPerson;
+	
+	private String phoneNumber;
+
+	private String gstNumber;
+	
+	private String supplierAddress;
+	
+	private LocalDate expectedDliveryDate;
+	
+	private String deliveryAddress;
+	
+	private String paymentTerms;
+	
+	private LocalDate paymentDueDate;
+	
+	private String termsAndCondition;
 }
